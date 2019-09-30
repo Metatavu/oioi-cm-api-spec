7c98ffdb0246a60a1b9d72754c3cf264002605f7
Antti Leppä <antti.leppa@metatavu.fi>
2019-09-30 07:53:43 +0300

    Added generated jaxrs

---
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/oioi/cm/rest/model/Device.java
@@ -0,0 +1,147 @@
+package fi.metatavu.oioi.cm.rest.model;
+
+import java.util.UUID;
+import java.util.List;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import fi.metatavu.oioi.cm.rest.model.*;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.*;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+
+public class Device   {
+  private @Valid UUID id = null;
+  private @Valid String name = null;
+  private @Valid OffsetDateTime createdAt = null;
+  private @Valid OffsetDateTime modifiedAt = null;
+
+  /**
+   * Device id.
+   **/
+  public Device id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  
+  //@ApiModelProperty(value = "Device id.")
+  @JsonProperty("id")
+
+  public UUID getId() {
+    return id;
+  }
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  /**
+   * Device name.
+   **/
+  public Device name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  
+  //@ApiModelProperty(required = true, value = "Device name.")
+  @JsonProperty("name")
+  @NotNull
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Creation time
+   **/
+  public Device createdAt(OffsetDateTime createdAt) {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  
+  //@ApiModelProperty(value = "Creation time")
+  @JsonProperty("createdAt")
+
+  public OffsetDateTime getCreatedAt() {
+    return createdAt;
+  }
+  public void setCreatedAt(OffsetDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  /**
+   * Last modification time
+   **/
+  public Device modifiedAt(OffsetDateTime modifiedAt) {
+    this.modifiedAt = modifiedAt;
+    return this;
+  }
+
+  
+  //@ApiModelProperty(value = "Last modification time")
+  @JsonProperty("modifiedAt")
+
+  public OffsetDateTime getModifiedAt() {
+    return modifiedAt;
+  }
+  public void setModifiedAt(OffsetDateTime modifiedAt) {
+    this.modifiedAt = modifiedAt;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Device device = (Device) o;
+    return Objects.equals(id, device.id) &&
+        Objects.equals(name, device.name) &&
+        Objects.equals(createdAt, device.createdAt) &&
+        Objects.equals(modifiedAt, device.modifiedAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, createdAt, modifiedAt);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Device {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    createdAt: ").append(toIndentedString(createdAt)).append("\n");
+    sb.append("    modifiedAt: ").append(toIndentedString(modifiedAt)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/oioi/cm/rest/model/ErrorResponse.java
@@ -0,0 +1,80 @@
+package fi.metatavu.oioi.cm.rest.model;
+
+import java.util.UUID;
+import java.util.List;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import fi.metatavu.oioi.cm.rest.model.*;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.*;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+
+public class ErrorResponse   {
+  private @Valid String message = null;
+
+  /**
+   * The error message
+   **/
+  public ErrorResponse message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  
+  //@ApiModelProperty(value = "The error message")
+  @JsonProperty("message")
+
+  public String getMessage() {
+    return message;
+  }
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ErrorResponse errorResponse = (ErrorResponse) o;
+    return Objects.equals(message, errorResponse.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(message);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ErrorResponse {\n");
+    
+    sb.append("    message: ").append(toIndentedString(message)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
